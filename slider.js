ea48c59a1947b7abfc1d93a4e6d42bc3868bcd56
Florian Egermann <florian@macht-medien.de>
2016-06-25 18:14:11 +0200

    removed logging

--- slider.js
+++ slider.js
@@ -1,8 +1,6 @@
 jQuery( document ).ready( function( $ ) {
 
   // available options: http://bxslider.com/options
-  
-  // console.log (options);
 
   // create slider for each gallery
   $('.swpgs-slider').each( function(  ) {
@@ -10,7 +8,6 @@ jQuery( document ).ready( function( $ ) {
     var slider_name = $( this ).attr('id');
     var slider_options_name = slider_name + '_args';
     var options = window[ slider_options_name ][0];
-    console.log (options);
     $(this).bxSlider( options);
 
     });
